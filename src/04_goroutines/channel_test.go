02c1053ceaeacb5c7534979786e45ad5b03ee2ad
Fajar Octhaviano <fajarduaa@gmail.com>
2023-09-08 16:11:05 +0700

    TestDeadlock

--- src/04_goroutines/channel_test.go
+++ src/04_goroutines/channel_test.go
@@ -3,6 +3,7 @@ package goroutines
 import (
 	"fmt"
 	"strconv"
+	"sync"
 	"testing"
 	"time"
 )
@@ -165,4 +166,56 @@ func TestRaceCondition(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 	fmt.Println("Counter =", x)
+}
+
+type UserBalance struct {
+	sync.Mutex
+	Name    string
+	Balance int
+}
+
+func (user *UserBalance) Lock() {
+	user.Mutex.Lock()
+}
+
+func (user *UserBalance) Unlock() {
+	user.Mutex.Unlock()
+}
+func (user *UserBalance) Change(amount int) {
+	user.Balance = user.Balance + amount
+}
+
+func Transfer(user1 *UserBalance, user2 *UserBalance, amount int) {
+	user1.Lock()
+	fmt.Println("Lock", user1.Name)
+	user1.Change(-amount)
+
+	time.Sleep(1 * time.Second)
+
+	user2.Lock()
+	fmt.Println("Lock", user2.Name)
+	user2.Change(amount)
+
+	time.Sleep(1 * time.Second)
+
+	user1.Unlock()
+	user2.Unlock()
+}
+
+func TestDeadlock(t *testing.T) {
+
+	user1 := UserBalance{
+		Name:    "Fajar",
+		Balance: 100000,
+	}
+
+	user2 := UserBalance{
+		Name:    "Ahmad",
+		Balance: 100000,
+	}
+
+	go Transfer(&user1, &user2, 1000)
+	go Transfer(&user2, &user1, 1000)
+
+	time.Sleep(5 * time.Second)
 }
