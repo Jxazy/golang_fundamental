ca78b64117ceeef642f9f9cc2ce6a4acb6c492d1
Fajar Octhaviano <fajarduaa@gmail.com>
2023-10-03 20:44:53 +0700

    Add response_code_test.go

---
+++ src/08_web/response_code_test.go
@@ -0,0 +1,50 @@
+package web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func ResponseCode(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+
+	if name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Name is empty")
+	} else {
+		fmt.Fprintf(w, "Hello %s", name)
+	}
+}
+
+func TestResponseCodeError(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseCode(recorder, request)
+
+	response := recorder.Result()
+	body, err := io.ReadAll(response.Body)
+	CheckError(err)
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
+
+func TestResponseCodeValid(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/?name=Fajar", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseCode(recorder, request)
+
+	response := recorder.Result()
+	body, err := io.ReadAll(response.Body)
+	CheckError(err)
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
