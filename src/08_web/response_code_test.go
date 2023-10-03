40a812928483b4b829b7c8d506493671fa7d22ae
Fajar Octhaviano <fajarduaa@gmail.com>
2023-10-04 06:24:01 +0700

    Add cookie_test.go (unfinished)

--- src/08_web/response_code_test.go
+++ src/08_web/response_code_test.go
@@ -20,6 +20,17 @@ func ResponseCode(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func ResponseCode2(w http.ResponseWriter, r *http.Request) {
+	jurusan := r.URL.Query().Get("jurusan")
+
+	if jurusan == "" {
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Fprint(w, "Jurusan is not found")
+	} else {
+		fmt.Fprintf(w, "Jurusan kamu : %s", jurusan)
+	}
+}
+
 func TestResponseCodeError(t *testing.T) {
 	request := httptest.NewRequest("GET", "http://localhost:8080", nil)
 	recorder := httptest.NewRecorder()
@@ -40,6 +51,21 @@ func TestResponseCodeValid(t *testing.T) {
 	recorder := httptest.NewRecorder()
 
 	ResponseCode(recorder, request)
+
+	response := recorder.Result()
+	body, err := io.ReadAll(response.Body)
+	CheckError(err)
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
+
+func TestResponseJurusan(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseCode2(recorder, request)
 
 	response := recorder.Result()
 	body, err := io.ReadAll(response.Body)
