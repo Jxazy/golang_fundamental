e2ccb2e94dd6a5e49b6ebda2d96ad4441d7e6082
Fajar Octhaviano <fajarduaa@gmail.com>
2023-10-06 06:07:56 +0700

    Add FileServe

---
+++ src/08_web/serve_file_test.go
@@ -0,0 +1,50 @@
+package web
+
+import (
+	_ "embed"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func ServeFile(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("name") != "" {
+		http.ServeFile(w, r, "./resources/ok.html")
+	} else {
+		http.ServeFile(w, r, "./resources/notfound.html")
+	}
+}
+
+func TestServeFileServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFile),
+	}
+
+	err := server.ListenAndServe()
+	CheckError(err)
+}
+
+//go:embed resources/ok.html
+var resourcesOK string
+
+//go:embed resources/notfound.html
+var resourcesNotFound string
+
+func ServeFileEmbed(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("name") != "" {
+		fmt.Fprint(w, resourcesOK)
+	} else {
+		fmt.Fprint(w, resourcesNotFound)
+	}
+}
+
+func TestServeFileEmbed(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFileEmbed),
+	}
+
+	err := server.ListenAndServe()
+	CheckError(err)
+}
