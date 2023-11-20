5ed51c9fc8dc4de0edc89e53e3f8155a8569394f
Fajar Octhaviano <fajarduaa@gmail.com>
2023-11-20 16:04:35 +0700

    add json validate

--- src/11_restful/main.go
+++ src/11_restful/main.go
@@ -1,27 +1,38 @@
 package main
 
 import (
-	"06_mysql/repository"
 	"11_restful/app"
 	"11_restful/controller"
+	"11_restful/helper"
+	"11_restful/repository"
 	"11_restful/service"
 	"github.com/go-playground/validator"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/julienschmidt/httprouter"
+	"net/http"
 )
 
 func main() {
 
 	db := app.NewDB()
 	validate := validator.New()
-	categoryRepository := repository.New
+	categoryRepository := repository.NewCategoryRepository()
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
 
 	router := httprouter.New()
 
 	router.GET("/api/categories", categoryController.FindAll)
-	router.GET("api/categories/:categoryId", categoryController.FindById)
-	router.POST("api/categories", categoryController.Create)
-	router.PUT(("api/categories/:categoryId", categoryController.Update)
-	router.DELETE("api/categories/:categoryId", categoryController.Delete)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.POST("/api/categories", categoryController.Create)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.CheckError(err)
 }
